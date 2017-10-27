22cb3d9c2395c00f7f71207b7f3626a8d07f232f
Noane Dan <Noane.Dan@outlook.com>
2017-10-27 10:04:18 +0300

    Add ray tracing

--- VirtualReality/Lab1/Sphere.cpp
+++ VirtualReality/Lab1/Sphere.cpp
@@ -1,13 +1,34 @@
 #include "Sphere.hpp"
+#include <iostream>
+#include <float.h>
 
 using namespace rt;
 
 Intersection Sphere::getIntersection(const Line& line, double minDist, double maxDist) {
-    Intersection in;
+  Intersection in;
 
-    // ADD CODE HERE
+  Vector poi;
+  poi = line.x0();
 
-    return in;
+  this->center();
+
+  // http://www.ambrsoft.com/TrigoCalc/Sphere/SpherLineIntersection_.htm
+  double a = (line.x0().x() - line.dx().x()) * (line.x0().x() - line.dx().x()) + (line.x0().y() - line.dx().y()) * (line.x0().y() - line.dx().y()) + (line.x0().z() - line.dx().z()) * (line.x0().z() - line.dx().z());
+  double b = 2 * ((line.x0().x() - line.dx().x()) * (line.dx().x() - this->_center.x()) + (line.x0().y() - line.dx().y()) * (line.dx().y() - this->_center.y()) + (line.x0().z() - line.dx().z()) * (line.dx().z() - this->_center.z()));
+  double c = this->_center.x() * this->_center.x() + this->_center.y() * this->_center.y() + this->_center.z() * this->_center.z() + line.dx().x() * line.dx().x() + line.dx().y() * line.dx().y() + line.dx().z() * line.dx().z() - 2 * (this->_center.x() * line.dx().x() + this->_center.y() * line.dx().y() + this->_center.z() * line.dx().z()) - this->_radius*this->_radius;
+  // condition for intersection
+  double delta = b * b - 4 * a * c;
+  double res;
+
+  if (delta < 0){
+    in =  Intersection(false, this, &line, DBL_MAX);
+  }
+  else {
+    res = -((-b + sqrt(delta)) / 2 * a);
+    in =  Intersection(true, this, &line, res);
+  }
+
+  return in;
 }
 
 
--- VirtualReality/Lab1/main.cpp
+++ VirtualReality/Lab1/main.cpp
@@ -10,6 +10,7 @@
 #include "Color.hpp"
 #include "Intersection.hpp"
 #include "Material.hpp"
+#include "stdio.h"
 
 #include "Scene.hpp"
 
@@ -17,17 +18,20 @@ using namespace std;
 using namespace rt;
 
 double imageToViewPlane(int n, int imgSize, double viewPlaneSize) {
-    double u = (double)n*viewPlaneSize / (double)imgSize;
-    u -= viewPlaneSize / 2;
-    return u;
+
+  double u = (double) n * viewPlaneSize / (double) imgSize;
+  u -= viewPlaneSize / 2;
+
+  return u;
 }
 
-const Intersection findFirstIntersection(const Line& ray,
-    double minDist, double maxDist) {
+const Intersection findFirstIntersection(const Line& ray, double minDist, double maxDist) {
+
     Intersection intersection;
 
-    for (int i = 0; i < geometryCount; i++) {
+    for (int i = 0; i < geometryCount; ++i) {
         Intersection in = scene[i]->getIntersection(ray, minDist, maxDist);
+
         if (in.valid()) {
             if (!intersection.valid()) {
                 intersection = in;
@@ -42,12 +46,12 @@ const Intersection findFirstIntersection(const Line& ray,
 }
 
 int main() {
+
     Vector viewPoint(0, 0, 0);
     Vector viewDirection(0, 0, 1);
     Vector viewUp(0, -1, 0);
 
-    double frontPlaneDist = 0;
-    double backPlaneDist = 1000;
+    Vector x0, x1;
 
     double viewPlaneDist = 512;
     double viewPlaneWidth = 1024;
@@ -55,8 +59,8 @@ int main() {
 
     int imageWidth = 1024;
     int imageHeight = 768;
-
-    Vector viewParallel = viewUp^viewDirection;
+    int i, j, k;
+    Vector viewParallel = viewUp ^ viewDirection;
 
     viewDirection.normalize();
     viewUp.normalize();
@@ -64,7 +68,62 @@ int main() {
 
     Image image(imageWidth, imageHeight);
 
-    // ADD CODE HERE
+    Color *c = new Color(0, 0, 0);
+
+    Vector n, t, e, r;
+
+    for (i = 0; i < imageWidth; ++i) {
+      for (j = 0; j < imageHeight; ++j) {
+        // background color
+        image.setPixel(i, j, Color(0.3, 0.3, 0.3));
+
+        // camera
+        x0 = viewPoint;
+        // point in space
+        x1 = viewPoint + viewDirection * viewPlaneDist + viewUp
+          * imageToViewPlane(j, imageHeight, viewPlaneHeight) + viewParallel
+          * imageToViewPlane(i, imageWidth, viewPlaneWidth);
+
+        // line from viewpoint to point in space
+        Line *line = new Line(x0, x1, true);
+
+        Intersection intersection = findFirstIntersection(*line, 0.25 , 0.25);
+        if (intersection.valid()) {
+          *c = intersection.geometry()->material().ambient();
+
+          for (k = 0; k < 2; ++k) {
+            *c *= lights[k]->ambient();
+
+            // normal to the surface
+            n = intersection.vec() - ((Sphere *)intersection.geometry())->center();
+            n.normalize();
+
+            // vector from intersection to light
+            t = lights[k]->position() - intersection.vec();
+            t.normalize();
+
+            if (n * t > 0) {
+              *c += intersection.geometry()->material().diffuse()*lights[k]->diffuse()*(n * t);
+            }
+
+            // vector from intersection point to camera
+            e = viewPoint - intersection.vec();
+            e.normalize();
+
+            // reflection vector
+            r = n * 2 * (n * t) - t;
+            r.normalize();
+
+            if(e * r > 0) {
+              *c += intersection.geometry()->material().specular() * lights[k]->specular() * pow(e * r, intersection.geometry()->material().shininess());
+            }
+            *c *= lights[k]->intensity();
+          }
+
+          image.setPixel(i, j, *c);
+        }
+      }
+    }
 
     image.store("scene.png");
 
